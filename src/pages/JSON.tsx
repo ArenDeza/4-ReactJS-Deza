453432f90df70f9f75179f1ee1e7cdc3a43dd27a
Aren Deza <apdeza1@up.edu.ph>
2024-02-06 15:03:52 +0800

    JSON

--- src/pages/JSON.tsx
+++ src/pages/JSON.tsx
@@ -1,19 +1,43 @@
 import Header from "../components/Header";
+import { Row, Col, Card } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
+import users from "https://jsonplaceholder.typicode.com/users";
 
 export default function JSON() {
-
-const Users = () => {
-
+    return (
+        <>
+        <h1>User Information</h1>
+        </>
+    )
 }
+
+/*
+export default function JSON() {
 
     return (
        <>
-       <Header />
        <h1>User Information</h1>
 
-
-
+       {
+        users && users.map( user: any => {
+            return(
+                <Col key={ user.id }>
+                { user.name }
+                
+                { user.address && user.address.map( local => {
+                    return(
+                        <div key={ user.id } >
+                            {local.street}
+                        </div>
+                        
+                    )
+                }) }
+                </Col>
+            )
+        }) 
+       }
        </>
     )
 }
+*/
+
